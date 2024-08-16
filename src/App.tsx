480757a86af13959b5d7d0bd98e65519ed1eb38d
Jason Park <jjpark987@gmail.com>
2024-08-16 16:21:37 -0700

    update useEffect and askChatbot in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-	// useEffect(() => {
-	// 	fetch('http://0.0.0.0:80/')
-	// 	.then(response => response.json)
-	// 	.then(initialMessage => )
-	// });
+interface Message {
+	text: string;
+	sender: 'user' | 'bot';
+	isPlaceholder?: boolean;
+}
 
+function App() {
 	const [isHidden, setIsHidden] = useState<boolean>(true);
+	const [messages, setMessages] = useState<Message[]>([]);
+	const [currentMessage, setCurrentMessage] = useState<string>('');
+
+	// Fetch the initial message
+	useEffect(() => {
+		fetch('http://0.0.0.0:80/')
+		.then(response => response.json())
+		.then(data => setMessages([{ text: data.message, sender: 'bot' }]))
+		.catch(error => {
+			setMessages([{ text: 'Server is down right now', sender: 'bot' }]);
+			console.error('Error fetching data:', error);
+		});
+	}, []);
+
+	// Send a POST request to API
+	function askChatbot() {
+    if (currentMessage.trim()) {
+			setMessages(prevMessages => [
+				...prevMessages,
+				{ text: currentMessage, sender: 'user' },
+				{ text: '...', sender: 'bot', isPlaceholder: true }
+			]);
+      setCurrentMessage('');
+      
+			fetch('http://0.0.0.0:80/ask', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					query: currentMessage
+				})
+			})
+			.then(response => response.json())
+			.then(data => {
+				setMessages(prevMessages => (
+					prevMessages.map(message =>
+							message.isPlaceholder ? { text: data.answer, sender: 'bot' } : message
+					)
+				));
+			})
+			.catch(error => {
+				setMessages([...messages, { text: 'Server is down right now', sender: 'bot' }]);
+				console.error('Error fetching data:', error);
+			});
+    }
+	}
 
 	return (
 		<div className='chatbot'>
@@ -19,7 +64,9 @@ function App() {
 						src='favicon.ico'
 						alt='keelbot_ico'
 					/>
-					<p>Have Questions?</p>
+					<div className='chatbot-header-msg'>
+						<p>Have Questions?</p>
+					</div>
 					<button 
 						className='chatbot-toggle'
 						onClick={() => setIsHidden(!isHidden)}
@@ -33,23 +80,27 @@ function App() {
 							src='favicon.ico'
 							alt='keelbot_ico'
 						/>
-						<p>You are chatting with: </p>
-						<h2>KeelBot</h2>
-						<p>We're online</p>
+						<div className='chatbot-header-msg'>
+							<h2>KeelBot</h2>
+						</div>
 						<button 
 							className='chatbot-toggle'
 							onClick={() => setIsHidden(!isHidden)}
 						>ᐯ</button>
 					</div>
 					<div className="chatbot-messages">
-						<div className="chatbot-message">
-							<p>Ask me anything about the KeelWorks Foundation!</p>
-						</div>
+						{messages.map((message, index) => (
+							<p key={index} className={message.sender}>
+								{message.text}
+							</p>
+						))}
 					</div>
 					<div className="chatbot-input">
 						<textarea
+							value = {currentMessage}
+							onChange={e => setCurrentMessage(e.target.value)}
 						/>
-						<button>Send</button>
+						<button onClick={askChatbot}>➤</button>
 					</div>
 				</div>
 			)}

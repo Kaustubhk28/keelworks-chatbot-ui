d16e2a2b1bc5f224d4b5d22feffeeb594a22bd8a
Jason Park <jjpark987@gmail.com>
2024-08-15 17:58:20 -0700

    update chatbot-container

--- src/App.tsx
+++ src/App.tsx
@@ -15,68 +15,45 @@ function App() {
 			{isHidden ? (
 				<div className='chatbot-header-hidden'>
 					<img 
+						className='chatbot-icon'
 						src='favicon.ico'
 						alt='keelbot_ico'
 					/>
 					<p>Have Questions?</p>
-					<button onClick={() => setIsHidden(!isHidden)}>
-						ᐱ
-					</button>
+					<button 
+						className='chatbot-toggle'
+						onClick={() => setIsHidden(!isHidden)}
+					>ᐱ</button>
 				</div>
 			) : (
 				<div className='chatbot-container'>
 					<div className='chatbot-header'>
-
+						<img 
+							className='chatbot-icon'
+							src='favicon.ico'
+							alt='keelbot_ico'
+						/>
+						<p>You are chatting with: </p>
+						<h2>KeelBot</h2>
+						<p>We're online</p>
+						<button 
+							className='chatbot-toggle'
+							onClick={() => setIsHidden(!isHidden)}
+						>ᐯ</button>
+					</div>
+					<div className="chatbot-messages">
+						<div className="chatbot-message">
+							<p>Ask me anything about the KeelWorks Foundation!</p>
+						</div>
+					</div>
+					<div className="chatbot-input">
+						<textarea
+						/>
+						<button>Send</button>
 					</div>
 				</div>
 			)}
 		</div>
-		// <div className='chatbot-container'>
-		// 	<div className='chatbot-header'>
-		// 		<img 
-		// 			src='favicon.ico'
-		// 			alt='keelbot_ico'
-		// ᐯ
-		// 		/>
-		// 		{isHidden ? (<p>Have Questions?</p>) : (<p>Chat with KeelBot</p>)}
-		// 		<button onClick={() => setIsHidden(!isHidden)}>
-		// 			{isHidden ? 'ᐱ' : 'ᐯ'}
-		// 		</button>
-		// 	</div>
-
-			
-
-		// 	{/* {isHidden ? (
-		// 		<div className='chatbot-header'>
-		// 			<p>Chat with me</p>
-		// 		</div>
-		// 	) : (
-		// 		<div className='chatbot-large'>
-		// 			<div className="chatbot-header">
-		// 				<img 
-		// 				src='favicon.ico'
-		// 				alt='keelbot_ico'
-		// 				width='50px'
-		// 				height='50px'
-		// 			/>
-		// 			<p>Chat with KeelBot</p>
-		// 				<p>You are chatting with: </p>
-		// 				<h2>KeelBot</h2>
-		// 				<p>We're online</p>
-		// 			</div>
-		// 			<div className="chatbot-messages">
-		// 					<div className="chatbot-message">
-		// 						<p>Ask me anything about the KeelWorks Foundation!</p>
-		// 					</div>
-		// 			</div>
-		// 			<div className="chatbot-input">
-		// 				<textarea
-		// 				/>
-		// 				<button>Send</button>
-		// 			</div>
-		// 		</div>
-		// 	)} */}
-		// </div>
 	);
 }
 
